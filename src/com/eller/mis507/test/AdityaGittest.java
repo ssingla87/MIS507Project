4db500e807b0d97e33027b91ec575ade841580de
Aditya <Aditya@10.142.136.221>
2017-10-28 16:00:54 -0700

    testing Github.

---
+++ src/com/eller/mis507/test/AdityaGittest.java
@@ -0,0 +1,14 @@
+package com.eller.mis507.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdityaGittest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
